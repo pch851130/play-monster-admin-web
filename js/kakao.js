37e322d1979364597cc77df3fba4dd11f331c25e
cheolhyun.park <pch851130@gmail.com>
2026-06-21 01:45:54 +0900

    Route Kakao token exchange through server auth endpoint

--- js/kakao.js
+++ js/kakao.js
@@ -1,7 +1,6 @@
 // 카카오 OAuth (REST, 브라우저 전용)
 window.Kakao = (function () {
   var AUTHORIZE_URL = "https://kauth.kakao.com/oauth/authorize";
-  var TOKEN_URL = "https://kauth.kakao.com/oauth/token";
   var LOGOUT_URL = "https://kapi.kakao.com/v1/user/logout";
 
   // 카카오 로그인(동의) 페이지로 이동
@@ -14,26 +13,22 @@ window.Kakao = (function () {
     window.location.href = AUTHORIZE_URL + "?" + params.toString();
   }
 
-  // authorization code -> access token 교환
+  // authorization code -> access token 교환.
+  // client_secret 이 켜져 있어 브라우저에서 직접 교환하지 않고,
+  // play-monster-server(/auth/kakao/token)가 시크릿을 붙여 대신 교환한다.
   async function exchangeToken(code) {
-    var params = new URLSearchParams({
-      grant_type: "authorization_code",
-      client_id: CONFIG.KAKAO_REST_API_KEY,
-      redirect_uri: CONFIG.REDIRECT_URI,
-      code: code,
-    });
-    var res = await fetch(TOKEN_URL, {
+    var res = await fetch(CONFIG.API_BASE_URL + "/auth/kakao/token", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded;charset=utf-8" },
-      body: params.toString(),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code: code, redirectUri: CONFIG.REDIRECT_URI }),
     });
-    var data = await res.json().catch(function () {
-      return {};
+    var body = await res.json().catch(function () {
+      return null;
     });
-    if (!res.ok || !data.access_token) {
-      throw new Error("카카오 토큰 교환 실패: " + (data.error_description || res.status));
+    if (!body || body.responseCode !== "OK" || !body.responseData || !body.responseData.accessToken) {
+      throw new Error("카카오 토큰 교환 실패: " + (body && body.errorMessage ? body.errorMessage : res.status));
     }
-    return data.access_token;
+    return body.responseData.accessToken;
   }
 
   // 카카오 로그아웃(토큰 만료). 실패해도 무시.
